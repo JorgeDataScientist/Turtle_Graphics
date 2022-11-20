fe89a060d744c4f31083b9a21ec4086285d66059
Jorgebuzo <Desarrollador@tutanota.com>
2022-11-20 15:58:32 -0500

    Turtle_Graphics

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+# TODO 1: Crear objeto 'timmy_the_tutle'
+timmy_the_tutle = Turtle()
+
+
+
+
+
+
+
+
+# TODO 2: Crear objeto para generar una ventana
+screen = Screen()
+screen.exitonclick()  #la ventana se cierra cuando hago click en ella
