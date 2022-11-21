be476dc8a3b63b273fc2b1b6064b877bce10af6e
Jorgebuzo <Desarrollador@tutanota.com>
2022-11-20 20:31:35 -0500

    Turtle_Graphics - Final

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Turtle_Graphics
 Simple exercise to generate graphical interfaces with the library:
 
 https://docs.python.org/3/library/turtle.html
+
+Click on a color below to see its turtle name, CSS name, hex code, or RGB values.
+
+https://trinket.io/docs/colors
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from turtle import Turtle, Screen
+import turtle as t
+from turtle import Screen
+import random
 
-# TODO 1: Crear objeto 'timmy_the_tutle'
-timmy_the_tutle = Turtle()
-timmy_the_tutle.shape('turtle')
-timmy_the_tutle.color('medium slate blue')
+tim = t.Turtle()
 
+########### Challenge 3 - Draw Shapes ########
 
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
+           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
 
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
-
-
-# TODO 2: Crear objeto para generar una ventana
 screen = Screen()
-screen.exitonclick()  #la ventana se cierra cuando hago click en ella
+screen.exitonclick()
